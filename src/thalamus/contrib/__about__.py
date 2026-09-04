199a7616d7953e21cfd4934d77b8d9ea4564e0bb
Jarl Haggerty <jarlhaggerty@gmail.com>
2026-09-04 19:59:28 -0400

    Use hatch for versioning

--- src/thalamus/contrib/__about__.py
+++ src/thalamus/contrib/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2026-present Jarl Haggerty <Jarl.Haggerty@pennmedicine.upenn.edu>
 #
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
+# SPDX-License-Identifier: GPL-3.0-only
+__version__ = "0.0.2"
